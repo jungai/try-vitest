bee236674b40b16ca7551793e6e387e2af1dfacc
jungai <juworaphol125@gmail.com>
2021-12-22 23:38:53 +0700

    update vite config

---
+++ vite.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  test: {
+    environment: "node",
+    global: true,
+    include: ["**/*.spec.{js,ts.vue,tsx}"],
+    // testTimeout: 5000 default lol
+    passWithNoTests: true,
+  },
+});
